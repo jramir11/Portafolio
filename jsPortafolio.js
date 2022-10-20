a62f72f21dccabdc5c31bbc13018c24e05ac23a4
Javier Ramirez <jramir11@bancoestado.cl>
2022-10-20 15:38:55 -0300

    commit inicial con hoja inicio y contacto

---
+++ jsPortafolio.js
@@ -0,0 +1,33 @@
+
+//para alternar Imagenes de Pantalla Inicial
+/*let cont=0
+function cambia() {
+    cont = cont % 2;
+    if (cont==1){
+        document.getElementById("fotocambia").src="fotos/IMG_20220217_103045.jpg";
+    }
+    else {
+        document.getElementById("fotocambia").src="fotos/IMG_20190520_144744 Orebro (26).jpg";
+    }
+    cont++;
+}
+function inicio() {
+    setInterval(cambia, 3000);
+}
+window.onload=inicio;
+*/
+
+/* scroll vertical
+window.onscroll = function() {myFunction()};
+var wInicio = document.getElementById("Inicio");
+var sticky = wInicio.offsetLeft;
+//offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    wInicio.classList.add("sticky");
+  } else {
+    wInicio.classList.remove("sticky");
+  }
+}
+*/
